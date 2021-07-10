b9e72799a4ec533ca55700c5edc906bd609273ae
nougatca <nougatca@outlook.com>
2021-07-10 20:07:35 +0800

    Remove functions not use

--- sources/utils/general.py
+++ sources/utils/general.py
@@ -1,15 +1,9 @@
 
-import torch
-
 from prettytable import PrettyTable
 
 
 def to_time(float_time):
-    """
-    translate float time to h, min, s and ms
-    :param float_time: time in float
-    :return: h, min, s, ms
-    """
+    """Translate float time to h, min, s and ms."""
     time_s = int(float_time)
     time_ms = int((float_time - time_s) * 1000)
     time_h = time_s // 3600
@@ -20,11 +14,13 @@ def to_time(float_time):
 
 
 def time2str(float_time):
+    """Transfer float time to string."""
     time_h, time_min, time_s, time_ms = to_time(float_time)
     return '{}h {}min {}s {}ms'.format(time_h, time_min, time_s, time_ms)
 
 
 def human_format(num):
+    """Transfer count number."""
     num = float('{:.3g}'.format(num))
     magnitude = 0
     while abs(num) >= 1000:
@@ -35,13 +31,12 @@ def human_format(num):
 
 
 def count_params(model):
-    """
-    Count the number of parameters of given model
-    """
+    """Count the number of parameters of given model."""
     return sum(p.numel() for p in model.parameters())
 
 
 def layer_wise_parameters(model):
+    """Returns a printable table representing the layer-wise model parameters, their shapes and numbers"""
     table = PrettyTable()
     table.field_names = ["Layer Name", "Output Shape", "Param #"]
     table.align["Layer Name"] = "l"
@@ -51,21 +46,3 @@ def layer_wise_parameters(model):
         if parameters.requires_grad:
             table.add_row([name, str(list(parameters.shape)), parameters.numel()])
     return table
-
-
-def init_module_params(module: torch.nn.Module):
-    """
-    Initialize the parameters of the given module
-    """
-    if isinstance(module, torch.nn.RNNBase):
-        for name, param in module.named_parameters():
-            if 'weight' in name:
-                torch.nn.init.xavier_normal_(param)
-            elif 'bias' in name:
-                torch.nn.init.normal_(param)
-    elif isinstance(module, torch.nn.Linear):
-        torch.nn.init.xavier_normal_(module.weight.data)
-        if module.bias is not None:
-            torch.nn.init.normal_(module.bias.data)
-    elif isinstance(module, torch.nn.Embedding):
-        torch.nn.init.uniform_(module.weight, -0.1, 0.1)
