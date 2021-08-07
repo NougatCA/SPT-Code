324b1820c67427395953459dabcf109cd43d3209
nougatca <nougatca@outlook.com>
2021-08-07 13:03:28 +0800

    Support for no ast or nl

--- sources/data/data_collator.py
+++ sources/data/data_collator.py
@@ -109,7 +109,9 @@ def collate_fn(batch, args, task, code_vocab, nl_vocab, ast_vocab):
             max_ast_len=args.max_ast_len,
             nl_raw=name_raw,
             nl_vocab=nl_vocab,
-            max_nl_len=args.max_nl_len
+            max_nl_len=args.max_nl_len,
+            no_ast=args.no_ast,
+            no_nl=args.no_nl
         )
 
         model_inputs['decoder_input_ids'], model_inputs['decoder_attention_mask'] = get_batch_inputs(
@@ -305,7 +307,8 @@ def get_batch_inputs(batch: List[str], vocab: Vocab, processor=None, max_len=Non
 
 def get_concat_batch_inputs(code_raw, code_vocab, max_code_len,
                             ast_raw, ast_vocab, max_ast_len,
-                            nl_raw, nl_vocab, max_nl_len):
+                            nl_raw, nl_vocab, max_nl_len,
+                            no_ast=False, no_nl=False):
     """
     Return the concat tensor and mask for input.
 
@@ -319,6 +322,8 @@ def get_concat_batch_inputs(code_raw, code_vocab, max_code_len,
         nl_raw:
         nl_vocab:
         max_nl_len:
+        no_ast:
+        no_nl:
 
     Returns:
         (torch.Tensor, torch.Tensor):
@@ -326,49 +331,46 @@ def get_concat_batch_inputs(code_raw, code_vocab, max_code_len,
             - concat attention mask
 
     """
-    # if code_raw:
-    #     code_inputs, code_padding_mask = get_batch_inputs(batch=code_raw,
-    #                                                       vocab=code_vocab,
-    #                                                       processor=Vocab.sep_processor,
-    #                                                       max_len=max_code_len)
-    # else:
-    #     code_inputs, code_padding_mask = None, None
-    #
-    # if ast_raw:
-    #     ast_inputs, ast_padding_mask = get_batch_inputs(batch=ast_raw,
-    #                                                     vocab=ast_vocab,
-    #                                                     processor=Vocab.sep_processor,
-    #                                                     max_len=max_ast_len)
-    # else:
-    #     ast_inputs, ast_padding_mask = None, None
-    #
-    # if nl_raw:
-    #     nl_inputs, nl_padding_mask = get_batch_inputs(batch=nl_raw,
-    #                                                   vocab=nl_vocab,
-    #                                                   processor=Vocab.eos_processor,
-    #                                                   max_len=max_nl_len)
-    # else:
-    #     nl_inputs, nl_padding_mask = None, None
-    #
-    # inputs = torch.cat([inputs for inputs in [code_inputs, ast_inputs, nl_inputs] if inputs is not None], dim=-1)
-    # padding_mask = torch.cat([mask for mask in [code_padding_mask, ast_padding_mask, nl_padding_mask]
-    #                           if mask is not None], dim=-1)
-
     code_inputs, code_padding_mask = get_batch_inputs(batch=code_raw,
                                                       vocab=code_vocab,
                                                       processor=Vocab.sep_processor,
                                                       max_len=max_code_len)
-    ast_inputs, ast_padding_mask = get_batch_inputs(batch=ast_raw,
-                                                    vocab=ast_vocab,
-                                                    processor=Vocab.sep_processor,
-                                                    max_len=max_ast_len)
-    nl_inputs, nl_padding_mask = get_batch_inputs(batch=nl_raw,
-                                                  vocab=nl_vocab,
-                                                  processor=Vocab.eos_processor,
-                                                  max_len=max_nl_len)
 
-    inputs = torch.cat([code_inputs, ast_inputs, nl_inputs], dim=-1)
-    padding_mask = torch.cat([code_padding_mask, ast_padding_mask, nl_padding_mask], dim=-1)
+    if not no_ast:
+        ast_inputs, ast_padding_mask = get_batch_inputs(batch=ast_raw,
+                                                        vocab=ast_vocab,
+                                                        processor=Vocab.sep_processor,
+                                                        max_len=max_ast_len)
+    else:
+        ast_inputs, ast_padding_mask = None, None
+
+    if not no_nl:
+        nl_inputs, nl_padding_mask = get_batch_inputs(batch=nl_raw,
+                                                      vocab=nl_vocab,
+                                                      processor=Vocab.eos_processor,
+                                                      max_len=max_nl_len)
+    else:
+        nl_inputs, nl_padding_mask = None, None
+
+    inputs = torch.cat([inputs for inputs in [code_inputs, ast_inputs, nl_inputs] if inputs is not None], dim=-1)
+    padding_mask = torch.cat([mask for mask in [code_padding_mask, ast_padding_mask, nl_padding_mask]
+                              if mask is not None], dim=-1)
+
+    # code_inputs, code_padding_mask = get_batch_inputs(batch=code_raw,
+    #                                                   vocab=code_vocab,
+    #                                                   processor=Vocab.sep_processor,
+    #                                                   max_len=max_code_len)
+    # ast_inputs, ast_padding_mask = get_batch_inputs(batch=ast_raw,
+    #                                                 vocab=ast_vocab,
+    #                                                 processor=Vocab.sep_processor,
+    #                                                 max_len=max_ast_len)
+    # nl_inputs, nl_padding_mask = get_batch_inputs(batch=nl_raw,
+    #                                               vocab=nl_vocab,
+    #                                               processor=Vocab.eos_processor,
+    #                                               max_len=max_nl_len)
+    #
+    # inputs = torch.cat([code_inputs, ast_inputs, nl_inputs], dim=-1)
+    # padding_mask = torch.cat([code_padding_mask, ast_padding_mask, nl_padding_mask], dim=-1)
 
     return inputs, padding_mask
 
