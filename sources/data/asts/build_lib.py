7d955601ec1851753ed159c4d253fdd6eea893c4
nougatca <nougatca@outlook.com>
2021-08-29 11:34:34 +0800

    Comment out git clones

--- sources/data/asts/build_lib.py
+++ sources/data/asts/build_lib.py
@@ -4,10 +4,9 @@ from tree_sitter import Language
 import subprocess
 import os
 
-
-for lang in ['go', 'javascript', 'python', 'java', 'php', 'ruby', 'c-sharp']:
-    subprocess.run(['git', 'clone', f'git@github.com:tree-sitter/tree-sitter-{lang}.git',
-                    f'vendor/tree-sitter-{lang}'])
+# for lang in ['go', 'javascript', 'python', 'java', 'php', 'ruby', 'c-sharp']:
+#     subprocess.run(['git', 'clone', f'git@github.com:tree-sitter/tree-sitter-{lang}.git',
+#                     f'vendor/tree-sitter-{lang}'])
 
 Language.build_library(
     # Store the library in the `build` directory
